5fa371f3255350f2bb381f6adac25d8cb7baf463
ts <shanksleslietrey@gmail.com>
2021-07-30 11:29:25 -0500

    price multiple added to vehicle types feature

---
+++ scripts/CarTypes.js
@@ -0,0 +1,31 @@
+import { getTypes, setType } from "./database.js"
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "type") {
+            setType(parseInt(event.target.value))
+        }
+    }
+)
+
+
+const types = getTypes()
+export const CarTypes = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    const listItemsArray = types.map(
+        (type) => {
+            return `<li>
+                <input type="radio" name="type" value="${type.id}" /> ${type.type} 
+                </li>`
+        }
+    )
+    // Join all of the strings in the array into a single string
+    html += listItemsArray.join(" ")
+
+    html += "</ul>"
+    return html
+}
+
